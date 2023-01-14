ae44dd3b29fb71385ae22c0aac35e54f478f3b89
bk147411@gmail.com <bk147411@gmail.com>
2023-01-14 21:54:38 +0530

    Structure change

--- components/MiniDrawer.tsx
+++ components/MiniDrawer.tsx
@@ -6,34 +6,31 @@ import {
   CSSObject,
   alpha,
 } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import InputBase from "@mui/material/InputBase";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import SearchIcon from "@mui/icons-material/Search";
-import Groups2Icon from "@mui/icons-material/Groups2";
 import {
+  Box,
+  Toolbar,
+  InputBase,
+  List,
+  CssBaseline,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   Avatar,
-  BottomNavigation,
-  BottomNavigationAction,
   Menu,
   MenuItem,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Groups2Icon from "@mui/icons-material/Groups2";
+import Image from "next/image";
 
 const drawerWidth = 240;
 
@@ -186,11 +183,7 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
-          <img
-            src="/Logo.png"
-            alt="Logo"
-            style={{ height: "10%", width: "10%" }}
-          />
+          <Image src="/Logo.png" alt="Logo" width="150" height="50" />
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
@@ -262,7 +255,7 @@ export default function MiniDrawer() {
                       justifyContent: "center",
                     }}
                   >
-                    {index % 2 === 0 ? <Groups2Icon /> : <MailIcon />}
+                    {index % 2 === 0 ? <Groups2Icon /> : "Mail"}
                   </ListItemIcon>
                   <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
                 </ListItemButton>
@@ -271,30 +264,6 @@ export default function MiniDrawer() {
           )}
         </List>
         <Divider />
-        {/* <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? "initial" : "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  {index % 2 === 0 ? <Groups2Icon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
