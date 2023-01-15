2743bf7e4523cd9b99ff5e4630aa2a224b31611f
Gurpreet <gurpreet-thakur@csgroup-chd.com>
2023-01-16 00:53:57 +0530

    code

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,25 +4,25 @@ import * as React from "react";
 import { Loginstyles } from "./styles";
 export default function Login() {
 
-return(
+  return (
     <Loginstyles>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <h2>Login</h2>
-      <TextField id="filled-basic" label="Email" variant="filled" />
-      <TextField id="filled-basic" label="Password" variant="filled" type="password" />
+      <Box
+        component="form"
+        className="form"
+        noValidate
+        autoComplete="off"
+      >
+        <img src="Images/Logoold.png" />
+        <h1>Welcome!</h1>
+        <h3>Sign Into Your Account</h3>
+        <TextField id="filled-basic" label="Email" variant="filled" />
+        <TextField id="filled-basic" label="Password" variant="filled" type="password" />
 
 
-       <Button variant="contained">Login</Button>
+        <Button variant="contained">Login</Button>
 
-    </Box>
-</Loginstyles>
-)
+      </Box>
+    </Loginstyles>
+  )
 
 }
--- src/pages/login/styles.tsx
+++ src/pages/login/styles.tsx
@@ -1,3 +1,69 @@
 import styled from "styled-components";
 
-export const Loginstyles = styled.div``;
+export const Loginstyles = styled.div`
+   top: 0;
+    width: 100%;
+    bottom: 0;
+    opacity: 1;
+    z-index: 999;
+    min-height: 100vh;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 0;
+    background: #efefef;
+    &::before{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 50%;
+        min-height: 100vh;
+        justify-content: center;
+        align-items: center;
+        padding: 30px 15px;
+        border-radius: 0 100% 100% 0;
+        background: #fff;
+        background: linear-gradient(132deg, #A31265, #572B73, #1D619F);
+        background-size: 400% 400%;
+        animation: Gradient 15s ease infinite;
+    }
+    form.form {
+    padding: 80px;
+    text-align: left;
+    position: relative;
+    background: #fff;
+    box-shadow: 0 0 10px rgb(0 0 0 / 10%);
+    border-radius: 10px;
+    max-width: 600px;
+    margin: 0 auto;
+    img {
+    width: 100%;
+    max-width: 200px;
+}
+}
+/* Animate Background*/
+@keyframes Gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+@keyframes cube {
+    from {
+        transform: scale(0) rotate(0deg) translate(-50%, -50%);
+        opacity: 1;
+    }
+    to {
+        transform: scale(20) rotate(960deg) translate(-50%, -50%);
+        opacity: 0;
+    }
+}
+`;
