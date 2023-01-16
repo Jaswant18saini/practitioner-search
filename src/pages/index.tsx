78f9637bf236f43724f6809068ce4fe02bc8c8ca
jaswant <jaswan-saini@csgroupchd.com>
2023-01-17 02:27:29 +0530

    updated_structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import withAuth from "@/Auth/withAuth"
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import LayoutWithLogin from "@/Layout";
+import React from "react";
 import DashboardPageComponent from "@/PagesComponents/Dashboard";
 
 const Home = () => {
   return (<DashboardPageComponent />);
-
 }
 
 export default withAuth(Home)
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,42 +1,15 @@
-import * as React from "react";
-import { Box, Button, TextField } from "@mui/material";
-import { Loginstyles } from "@/PagesComponents/Login/Login.style";
 import withoutAuth from "@/Auth/withoutAuth";
-import { NextRouter } from "next/router";
-
-interface ILoginProps {
-  router: NextRouter
-}
+import ILoginProps from "@/PagesComponents/Login/Login.interface";
+import useLoginHook from "@/hooks/login.hook";
+import LoginComponent from "@/PagesComponents/Login";
 
 const Login = (props: ILoginProps) => {
-  const { router } = props;
-
-  const [userData, setUserData] = React.useState<{ email: string, password: string }>({ email: "", password: "" });
-
-  function _handleLogin() {
-    console.log(userData, "userData>");
-    const { email, password } = userData;
-    if (email !== "" && password !== "") {
-      localStorage.setItem('Login', 'true');
-      router.push('/dashboard');
-    }
-  }
-
-  function onHandleChange(e: any, section: string): void {
-    setUserData((prv) => ({ ...prv, [section]: e.target.value }))
-  }
+  const [
+    _handleLogin,
+  ] = useLoginHook(props);
 
   return (
-    <Loginstyles>
-      <Box component="form" className="form" noValidate autoComplete="off">
-        <img src="Images/Logoold.png" />
-        <h1>Welcome!</h1>
-        <h3>Sign Into Your Account</h3>
-        <TextField label="Email" variant="filled" value={userData?.email} onChange={(e) => onHandleChange(e, "email")} />
-        <TextField label="Password" variant="filled" value={userData?.password} type="password" onChange={(e) => onHandleChange(e, "password")} />
-        <Button variant="contained" type="button" onClick={() => _handleLogin()}>Login</Button>
-      </Box>
-    </Loginstyles>
+    <LoginComponent {...{ _handleLogin }} />
   );
 }
 
